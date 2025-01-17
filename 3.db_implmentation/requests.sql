c84c3abbd3dddd44bda1d6dffecf3e16ab69767b
denysprokhorin <prokhorind@gmail.com>
2025-01-17 16:36:41 +0200

    add requests example

---
+++ 3.db_implmentation/requests.sql
@@ -0,0 +1,32 @@
+SELECT * FROM Cities;
+
+SELECT Name FROM Superheroes;
+
+SELECT Name, RealName FROM Superheroes;
+
+
+SELECT Villains.Name
+FROM Villains
+INNER JOIN Cities ON Villains.CityID = Cities.CityID
+WHERE Cities.Name = 'Metropolis';
+
+
+SELECT Cities.Name, COUNT(Superheroes.SuperheroID) AS HeroCount
+FROM Cities
+LEFT JOIN Superheroes ON Cities.CityID = Superheroes.CityID
+GROUP BY Cities.Name;
+
+
+
+
+SELECT 'Superhero' AS Type, Superheroes.Name AS Name
+FROM Superheroes
+INNER JOIN Cities ON Superheroes.CityID = Cities.CityID
+WHERE Cities.Name = 'Gotham'
+
+UNION
+
+SELECT 'Villain' AS Type, Villains.Name AS Name
+FROM Villains
+INNER JOIN Cities ON Villains.CityID = Cities.CityID
+WHERE Cities.Name = 'Gotham';
